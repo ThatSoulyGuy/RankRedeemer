ea02c86eba6e22d8752ab87667ba2d25539a3cea
ThatSoulyGuy <bkmcman10bk@gmail.com>
2024-03-10 19:46:12 -0500

    Fixed commands and events

--- src/main/java/com/thatsoulyguy/rankredeemer/RankRedeemer.java
+++ src/main/java/com/thatsoulyguy/rankredeemer/RankRedeemer.java
@@ -1,5 +1,7 @@
 package com.thatsoulyguy.rankredeemer;
 
+import com.thatsoulyguy.rankredeemer.command.CommandManager;
+import com.thatsoulyguy.rankredeemer.event.EventPlayer;
 import com.thatsoulyguy.rankredeemer.util.RRConfig;
 import net.luckperms.api.LuckPerms;
 import org.bukkit.Bukkit;
@@ -22,6 +24,9 @@ public final class RankRedeemer extends JavaPlugin
 
         if (provider != null)
             luckPerms = provider.getProvider();
+
+        getServer().getPluginManager().registerEvents(new EventPlayer(), this);
+        getCommand("rankredeemer").setExecutor(new CommandManager());
 
         defaultConfig.Initialize("config.yml");
     }
